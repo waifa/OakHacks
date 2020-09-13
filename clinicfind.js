0e0ad12bbb40952f7ebade1d29bc64397403c7db
Muhammad Moaz <m.moaz@mail.utoronto,ca>
2020-09-12 21:52:26 -0400

    added slider for distance to search within

--- clinicfind.js
+++ clinicfind.js
@@ -5,6 +5,7 @@ var clinics = []
 var tableDiv = "body"
 var postalCode
 var click = false;
+var sliderValue = 50;
 
 
 function clinicFind(){
@@ -15,6 +16,16 @@ function clinicFind(){
             var searchbar = "<input type='text' id='searchInput' placeholder='"+placeholder+"' name='search'>" +
                             "<button class='searchButton' type='submit' onclick='clinicFind().sortClinics()'><img width='10px' height='10px' src='searchIcon.png'></button>"
             $(selector).append(searchbar)
+            // slider is not my work and is borrowed straight from w3schools
+            var slider  = '<input type="range" min="1" max="100" value="50" class="slider" id="myRange"><p>Search Within: <span id="demo"></span> km</p>'
+            $(selector).append(slider)
+            var slider = document.getElementById("myRange");
+            var output = document.getElementById("demo");
+            output.innerHTML = slider.value;
+            slider.oninput = function() {
+                output.innerHTML = this.value;
+                sliderValue = this.value
+            }
         },
 
 
@@ -33,6 +44,7 @@ function clinicFind(){
             if (!click){
             click = true
             async function getLatLng(){
+                let notValid = false;
                 let lat
                 let lng
                 postalCode = $("#searchInput").val()
@@ -43,8 +55,10 @@ function clinicFind(){
                     lat = data.results[0].geometry.location.lat
                     lng = data.results[0].geometry.location.lng
                 }else{
+                    notValid = true;
                     alert("Not a Valid Postal Code")
                 }
+                if (!notValid){
                 let lat2
                 let lng2
                 let url2
@@ -68,10 +82,16 @@ function clinicFind(){
                     }
                     return 0
                 })
+                for(var i = 0; i<clinics.length; i++){
+                    if (clinics[i].distance > sliderValue){
+                        clinics.splice(i)
+                    }
+                }
 
                 self.createTable(tableDiv)
             }
-            getLatLng()
+        }
+                getLatLng()
             }
         },
 
@@ -80,19 +100,28 @@ function clinicFind(){
         },
 
         createTable: (selector)=>{
-            var table ="<table><tr><th><h3>Clinic</h3></th><th><h3>Location</h3></th><th><h3>Distance From You</h3></th><th><h3>Directions</h3></th></tr>"
-            for(var i = 0; i<clinics.length; i++){
-                var link = "onclick=\"window.open('https://www.google.com/maps/search/"+clinics[i].location+"');\""
-                table = table + "<tr> <td><b>" +clinics[i].name+ "</b></td>" + 
-                "<td>"+clinics[i].location+"</td><td>"+ Math.round(clinics[i].distance * 10) / 10 +" Km</td>" +
-                "<td><button class='directionButton'" +  link + ">Directions</button></td></tr>"
+            if (clinics.length>0){
+                var table ="<table><tr><th><h3>Clinic</h3></th><th><h3>Location</h3></th><th><h3>Distance From You</h3></th><th><h3>Directions</h3></th></tr>"
+                for(var i = 0; i<clinics.length; i++){
+                    var link = "onclick=\"window.open('https://www.google.com/maps/search/"+clinics[i].location+"');\""
+                    table = table + "<tr> <td><b>" +clinics[i].name+ "</b></td>" + 
+                    "<td>"+clinics[i].location+"</td><td>"+ Math.round(clinics[i].distance * 10) / 10 +" Km</td>" +
+                    "<td><button class='directionButton'" +  link + ">Directions</button></td></tr>"
+                }
+                table = table + "</table>"
+                $(selector).append("<h2>Clinics Close to You:</h2>").hide().show(300)
+                $(selector).append(table).hide().show(300)
+                var searchAgain = "<button class='directionButton' onclick='window.location.reload()'>Search Again</button>"
+                $(selector).append(searchAgain).hide().show(300)
+                $("#round-container").hide(300)
+            }else{
+                var error = "<h2>Sorry, no clinics were found with your search criteria :("
+                $(selector).append(error).hide().show(300)
+                var searchAgain = "<button class='directionButton' onclick='window.location.reload()'>Search Again</button>"
+                $(selector).append(searchAgain).hide().show(300)
+                $("#round-container").hide(300)
             }
-            table = table + "</table>"
-            $(selector).append("<h2>Clinics Close to You:</h2>").hide().show(300)
-            $(selector).append(table).hide().show(300)
-            var searchAgain = "<button class='directionButton' onclick='window.location.reload()'>Search Again</button>"
-            $(selector).append(searchAgain).hide().show(300)
-            $("#round-container").hide(300)
+            
         },
 
         //distance function from https://www.geodatasource.com/developers/javascript
